52f89561d2ecb3f865ed0f563fd7bc5331cc7caa
julianloaiza <loaizajulian1999@gmail.com>
2025-09-16 20:30:12 -0500

    Base de datos SQL, Entidad y Rest básico

---
+++ platzi-play/src/main/java/com/platzi/play/persistence/entity/MovieEntity.java
@@ -0,0 +1,40 @@
+package com.platzi.play.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "platzi_play_peliculas")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class MovieEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150, unique = true)
+    private String titulo;
+
+    @Column(nullable = false, precision = 3)
+    private Integer duracion;
+
+    @Column(nullable = false, length = 40)
+    private String genero;
+
+    @Column(name = "fecha_estreno")
+    private LocalDate fechaEstreno;
+
+    @Column(precision = 3, scale = 2)
+    private BigDecimal clasificacion;
+
+    @Column(nullable = false, length = 1)
+    private String estado;
+}
